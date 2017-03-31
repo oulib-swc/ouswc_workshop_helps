ee54a3172220eced0bfa73d1eaa5683e86edfbc9
Mark Laufersweiler <laufers@gmail.com>
2017-03-31 09:22:47 -0500

    mk_attend: reversed loop order template: edits

--- mk_attend_list.py
+++ mk_attend_list.py
@@ -7,7 +7,8 @@ import csv
 # orders = pd.read_csv("report-2016-10-25T2057.csv", index_col='Order #')
 
 orders = ''
-file1 = ''
+file1 = '2017_03_13_ou/report-2017-03-15T1523.csv'
+
 while file1 == '':
     file1 = input('Enter report filename from EventBrite: ')
 
@@ -18,33 +19,34 @@ orders = pd.read_csv(file1, index_col='Order #')
 #attendees = pd.read_csv('attendees_2016-10-25-ou.txt', delimiter=' ', header=None, names=['first_name', 'last_name'])
 
 attendees = ''
-file2 = ''
+file2 = '2017_03_13_ou/2017-03-13-sign-in_list.txt'
 while file2 == '':
 	file2 = input('Enter list of attendees from etherpad: ')
 
-print(file2)
 attendees = pd.read_csv(file2, delimiter=' ', header=None, names=['first_name', 'last_name'])
 
-
-# In[15]:
-
+# Form lists for comparison
 attend_list = attendees['last_name'].tolist()
 order_list = orders['Last Name'].tolist()
 
-# open files for ouput as csv file
+# make lists lower case
+attend_list = [item.lower() for item in attend_list]
+order_list = [item.lower() for item in order_list]
 
+
+# open files for ouput as csv file
 out = open('final_attendenc.csv', 'w')
 wr = csv.writer(out, dialect='excel')
 wr.writerow(['name', 'email', 'department'])
 
-
+# test for EventBrite sign-up with attendee sign-in
 for index, row in orders.iterrows():
-    if row['Last Name'] in attend_list:
+    if row['Last Name'].lower() in attend_list:
         print (row['First Name'], row['Last Name'], '/', row['Email'], '/', row['Department or Center'] )
         wr.writerow([row['First Name'] + ' ' +row['Last Name'],  row['Email'],  row['Department or Center'] ])
-    else:
-        print (row['First Name'], row['Last Name'], '/', row['Email'], '/', row['Department or Center'] )
-        wr.writerow([row['First Name'] + ' ' +row['Last Name']])
+#    else:
+#        print (row['First Name'], row['Last Name'], '/', row['Email'], '/', row['Department or Center'] )
+#        wr.writerow([row['First Name'] + ' ' +row['Last Name']])
 
 
 out.close()
--- ou_swc_report_templet.md
+++ ou_swc_report_templet.md
@@ -1,17 +1,18 @@
 Carpentry Workshop Event Report
 
-What: 	Software or Data Carpentry 
+What: 	Software or Data Carpentry
 Title:	[2016-06-15-ou]
 Date:	[June 15-16, 2016]
 Who:	University Libraries
 Where:	University of Oklahoma
+Website:	[website url]
 
 Attendance
 
 	Total Registered:
 	Attendance:
-		Day 1:	
-		Day 2:	
+		Day 1:
+		Day 2:
 
 
 Curriculum
@@ -21,14 +22,9 @@ Narrative (thorns and roses)
 
 
 Instructors
-	
+
 
 Helpers
-	
+
 Learners
 	name , email , department/domain
-
-
-
-
-
