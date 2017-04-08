4e05eadb1697c09f47e3ca9e0ac6886b4554156b
Mark Laufersweiler <laufers@gmail.com>
2017-04-08 13:57:33 -0500

    added error traping and help

--- mk_attend_list.py
+++ mk_attend_list.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python
 import pandas as pd
 import csv
+import argparse
+import sys
+
+parser=argparse.ArgumentParser(
+    description='''This script compairs the EventBrite registration with
+                    the etherpad sign-ins and outputs a final attendance
+                    list with emails and domains if available.
+
+                
+                    The list of learners needs to be edited to only include
+                    the first and last names of the attendees''',
+    epilog="""Thank you for playing.""")
+args=parser.parse_args()
 
 
 # Read in csv file from EventBrite
-# orders = pd.read_csv("report-2016-10-25T2057.csv", index_col='Order #')
-
 orders = ''
-# file1 = './report-2017-03-15T1523.csv'
 file1 = ''
-
-while file1 == '':
-    file1 = input('Enter report filename from EventBrite: ')
-
-print(file1)
-orders = pd.read_csv(file1, index_col='Order #')
+try:
+    while file1 == '':
+        file1 = input('Enter report filename from EventBrite: ')
+    orders = pd.read_csv(file1, index_col='Order #')
+except:
+    print (file1, "does not exist")
+    sys.exit()
 
 # Read in file list of learners from etherpad (first name last name)
-#attendees = pd.read_csv('attendees_2016-10-25-ou.txt', delimiter=' ', header=None, names=['first_name', 'last_name'])
-
 attendees = ''
 # file2 = './2017-03-13-sign-in_list.txt'
 file2 = ''
-while file2 == '':
-	file2 = input('Enter list of attendees from etherpad: ')
+try:
+    while file2 == '':
+    	file2 = input('Enter list of attendees from etherpad: ')
 
-attendees = pd.read_csv(file2, delimiter=' ', header=None, names=['first_name', 'last_name'])
+    attendees = pd.read_csv(file2, delimiter=' ', header=None, names=['first_name', 'last_name'])
+except:
+    print(file2, 'does not exist')
+    sys.exit()
 
 # Form lists for comparison
 attend_list = attendees['last_name'].tolist()
