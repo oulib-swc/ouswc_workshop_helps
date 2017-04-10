def805d23cfe4584c96b26551edb6012fed8dc99
Mark Laufersweiler <laufers@gmail.com>
2017-04-10 09:51:30 -0500

    clean up and add comments

--- mk_attend_list.py
+++ mk_attend_list.py
@@ -21,37 +21,32 @@ args=parser.parse_args()
 
 # Read in csv file from EventBrite
 orders = ''
-#file1 = ''
 try:
     orders = pd.read_csv(args.file1, index_col='Order #')
 except:
-    print (args.file1, "does not exist")
+    print ('There is a problem with', args.file1)
     sys.exit()
 
 # Read in file list of learners from etherpad (first name last name)
 attendees = ''
-#file2 = ''
-
-
 try:
-#    while file2 == '':
-#    	args.file2 = input('Enter list of attendees from etherpad: ')
-
     attendees = pd.read_csv(args.file2, delimiter=' ', header=None, names=['first_name', 'last_name'] + list(range(0,4)))
-#    attendees = pd.read_csv(args.file2, delimiter=' ', header=None, names=['first_name', 'last_name', 'other0', 'other1', 'other2', 'other3', 'other4'])
-#    attendees = pd.read_csv(args.file2, delimiter=' ', header=None)
-#    attendees.columns = ['first_name', 'last_name', 'other']
+    # list(range) is needed to account for the copied sign-in list not have the
+    # same number of columns for each sign-in. This also is assuming the name fields
+    # to only be two parts, first and last and they are the first two items of
+    # a sign-in entry.
 except:
-    print(args.file2, 'does not exist')
+    print('There is a problem with', args.file2)
     sys.exit()
-print(attendees)
-
 
 # Form lists for comparison
+# This will be problematic if two learners have the same last name. At that point
+# I will need to find another search method for comaring the two lists. It also
+# does not take into account typos in the entries by the registrant.
 attend_list = attendees['last_name'].tolist()
 order_list = orders['Last Name'].tolist()
 
-# make lists lower case
+# make both lists lower case
 attend_list = [item.lower() for item in attend_list]
 order_list = [item.lower() for item in order_list]
 
@@ -61,25 +56,16 @@ out = open('final_attendenc.csv', 'w')
 wr = csv.writer(out, dialect='excel')
 wr.writerow(['name', 'email', 'department'])
 
-# test for EventBrite sign-up with attendee sign-in
-# for index, row in orders.iterrows():
-#     if row['Last Name'].lower() in attend_list:
-#         print (row['First Name'], row['Last Name'], '/', row['Email'], '/', row['Department or Center'] )
-#         wr.writerow([row['First Name'] + ' ' +row['Last Name'],  row['Email'],  row['Department or Center'] ])
-#     else:
-#         print (row['First Name'], row['Last Name'], 'no email or department available' )
-#         wr.writerow([row['First Name'] + ' ' +row['Last Name']])
-
-# Check for learns who pre-registered
+# Check pre-registered in the sign-in list
 for index, row in orders.iterrows():
     if row['Last Name'].lower() in attend_list:
         print (row['First Name'], row['Last Name'], '/', row['Email'], '/', row['Department or Center'] )
         wr.writerow([row['First Name'] + ' ' +row['Last Name'],  row['Email'],  row['Department or Center'] ])
-# Now check for learner who attended and did not pre-register
+# Now check sign-in learner who may not have pre-register. Walk-ins.
 for index,row in attendees.iterrows():
     if row['last_name'].lower() not in order_list:
         print (row['first_name'], row['last_name'], 'no email or department available' )
         wr.writerow([row['first_name'] + ' ' +row['last_name']])
 
-
+# Close output file. 
 out.close()
